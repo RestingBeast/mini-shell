07596e549a629a64ad21ada635ea8e7ea54edc5b
RestingBeast <mmmike2002@gmail.com>
2026-07-16 09:04:45 +0800

    WIP: Writing Basic Tests
    Too many tests.

---
+++ tests/helpers.c
@@ -0,0 +1,118 @@
+#include "unit_test.h"
+
+const char	*quote_name(enum Quote q)
+{
+	switch (q)
+	{
+		case 0:
+			return ("NONE");
+		case 1:
+			return ("SINGLE");
+		case 2:
+			return ("DOUBLE");
+	}
+	return (NULL);
+}
+
+const char	*type_name(enum Type t)
+{
+	switch (t)
+	{
+		case 0:
+			return ("WORD");
+		case 1:
+			return ("PIPE");
+		case 2:
+			return ("REDIRECT_IN");
+		case 3:
+			return ("REDIRECT_OUT");
+		case 4:
+			return ("HEREDOC");
+		case 5:
+			return ("APPEND");
+	}
+	return (NULL);
+}
+
+int	compare_segment(t_segment **out, t_segment **exp)
+{
+	int	i;
+
+	if (out == NULL)
+		return (0);
+	i = 0;
+	while (out[i] != NULL || exp[i] != NULL)
+	{
+		if (out[i] == NULL || exp[i] == NULL)
+		{
+			printf(
+				"Segment #%d\n\tLexeme Length is not the same\n"
+				"",
+				(i + 1)
+			);
+			return (0);
+		}
+		if (out[i]->quote != exp[i]->quote)
+		{
+			printf(
+				"Segment #%d\n"
+				"    Expected quote: %s\n"
+				"    Actual quote:   %s\n",
+				i + 1,
+				quote_name(exp[i]->quote),
+				quote_name(out[i]->quote)
+			);
+			return (0);
+		}
+		if (strcmp(out[i]->text, exp[i]->text) != 0)
+		{
+			printf(
+				"Segment #%d\n"
+				"    Expected text: %s\n"
+				"    Actual text:   %s\n",
+				i + 1,
+				exp[i]->text,
+				out[i]->text
+			);
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	compare_token(t_token **out, t_token **exp)
+{
+	int	i;
+
+	if (out == NULL)
+		return (0);
+	i = 0;
+	while (out[i] != NULL || exp[i] != NULL)
+	{
+		if (out[i] == NULL || exp[i] == NULL)
+		{
+			printf(
+				"Token #%d\n\tToken Length is not the same\n",
+				(i + 1)
+			);
+			return (0);
+		}
+		if (out[i]->type != exp[i]->type)
+		{
+			printf(
+				"Token #%d\n"
+				"    Expected type: %s\n"
+				"    Actual type:   %s\n",
+				i + 1,
+				type_name(exp[i]->type),
+				type_name(out[i]->type)
+			);
+			return (0);
+		}
+		if (!compare_segment(out[i]->lexeme, exp[i]->lexeme))
+			return (0);
+		i++;
+	}
+	return (1);
+}
